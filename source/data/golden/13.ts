c614ae0106fbe11cf7bf0c15bc0bd0ba359fe08d
Eaxon <aeurum@gmail.com>
2025-04-08 00:36:50 +0300

    Create 13.ts

---
+++ source/data/golden/13.ts
@@ -0,0 +1,40 @@
+import { GoldenPrimes, GoldenInverses } from '../../interfaces'
+
+export const primes13: GoldenPrimes = [
+  BigInt('2'),
+  BigInt('11'),
+  BigInt('107'),
+  BigInt('1361'),
+  BigInt('17657'),
+  BigInt('229487'),
+  BigInt('2983139'),
+  BigInt('38780717'),
+  BigInt('504149341'),
+  BigInt('6553941047'),
+  BigInt('85201233643'),
+  BigInt('1107616036837'),
+  BigInt('14399008478509'),
+  BigInt('187187110220261'),
+  BigInt('2433432432863387'),
+  BigInt('31634621627224111'),
+  BigInt('411250081153910567')
+]
+export const inverses13: GoldenInverses = [
+  BigInt('0'),
+  BigInt('6'),
+  BigInt('139'),
+  BigInt('770'),
+  BigInt('17793'),
+  BigInt('4946'),
+  BigInt('3230873'),
+  BigInt('53485004'),
+  BigInt('479455745'),
+  BigInt('6116601228'),
+  BigInt('56042848808'),
+  BigInt('1234269361955'),
+  BigInt('3230380485977'),
+  BigInt('26179300841010'),
+  BigInt('3896972692066313'),
+  BigInt('41607168365184312'),
+  BigInt('222703042258456608')
+]
