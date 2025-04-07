a57bfec5fa4208ad38ab8316a03fb42347a97e98
Eaxon <aeurum@gmail.com>
2025-04-08 00:27:29 +0300

    Added tests for `BigInt`

--- test/pseudo-crypto.test.js
+++ test/pseudo-crypto.test.js
@@ -1,35 +1,84 @@
-import { PseudoCrypto } from '../source/pseudo-crypto'
+const PseudoCrypto = require('../dist/main.js')
 
-const pseudo_crypto = {
+const pseudoCrypto = {
   numeric: new PseudoCrypto('numeric'),
-  uppercase: new PseudoCrypto('uppercase'),
+  inline: new PseudoCrypto('inline'),
+  wide: new PseudoCrypto('wide'),
   lowercase: new PseudoCrypto('lowercase'),
-  lowercase_wide: new PseudoCrypto('l-'),
-  lowercase_inline: new PseudoCrypto('l_'),
+  uppercase: new PseudoCrypto('uppercase'),
   alphabetic: new PseudoCrypto('alphabetic'),
   alphanumeric: new PseudoCrypto('alphanumeric')
 }
 
+const max = BigInt(Number.MAX_SAFE_INTEGER)
+
 function random(min, max) {
+  if (typeof max === 'bigint')
+    return randomBigInt(min, max)
+  else return randomNumber(min, max)
+}
+function randomBigInt(min, max) {
+  const range = max - min + 1n
+
+  let bits = 0n
+  let temporaryRange = range
+  while (temporaryRange > 0n) {
+    bits++
+    temporaryRange >>= 1n
+  }
+
+  let addend
+  do {
+    addend = 0n
+    let bitsLeft = bits
+
+    while (bitsLeft > 0n) {
+      const bitsToGenerate = bitsLeft > 32n ? 32n : bitsLeft
+      const random = Math.random()
+      const ceil = Number(2n ** bitsToGenerate)
+      const fragment = BigInt(Math.floor(random * ceil))
+      addend = (addend << bitsToGenerate) | fragment
+      bitsLeft -= bitsToGenerate
+    }
+  } while (addend >= range)
+  return min + addend
+}
+function randomNumber(min, max) {
   return Math.round(Math.random() * (max - min) + min)
 }
 
+function text(i, t, v, n, l) {
+  return `I ${i}: ${t} hash/unhash for ${v} ${n} and len ${l}`
+}
+
+let index = 0
 for (const data of [
   { type: 'numeric', cc: 10 },
+  { type: 'inline', cc: 13 },
+  { type: 'wide', cc: 21 },
   { type: 'uppercase', cc: 26 },
   { type: 'lowercase', cc: 26 },
-  { type: 'lowercase_wide', cc: 21 },
-  { type: 'lowercase_inline', cc: 13 },
   { type: 'alphabetic', cc: 52 },
   { type: 'alphanumeric', cc: 62 }
 ]) {
-  for (let i = 0; i < 1000; i++) {
-    const length = random(1, 16)
-    const current_limit = BigInt(data.cc ** length - 1)
-    const max_integer = BigInt(Number.MAX_SAFE_INTEGER)
-    const number = random(1, Number(current_limit <= max_integer ? current_limit : max_integer))
-    test(`Iteration ${i}: ${data.type} hash/unhash for num ${number} and len ${length}`, () => {
-      expect(pseudo_crypto[data.type].unhash(pseudo_crypto[data.type].hash(number, length))).toBe(number)
-    })
+  for (let i = 0; i < 17; i++) {
+    for (let l = 1; l < 17; l++) {
+      const lim = BigInt(data.cc) ** BigInt(l) - BigInt(1)
+      const end = Number(lim <= max ? lim : max)
+      const num = random(1, end)
+      const int = random(BigInt(1), lim)
+      test(text(++index, data.type, 'num', num, l), () => {
+        const hash = pseudoCrypto[data.type].hash(num, l)
+        expect(pseudoCrypto[data.type].unhash(hash)).toBe(num)
+      })
+      test(text(++index, data.type, 'int', int, l), () => {
+        const hash = pseudoCrypto[data.type].hash(int, l)
+        expect(BigInt(pseudoCrypto[data.type].unhash(hash))).toBe(int)
+      })
+      test(text(++index, data.type, 'lim', lim, l), () => {
+        const hash = pseudoCrypto[data.type].hash(lim, l)
+        expect(BigInt(pseudoCrypto[data.type].unhash(hash))).toBe(lim)
+      })
+    }
   }
 }
