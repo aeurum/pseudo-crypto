ea85011a7bee4edf659fe0e837b0110cdbaa6883
Eaxon <aeurum@gmail.com>
2025-04-08 00:37:26 +0300

    Create 26.ts

---
+++ source/data/golden/26.ts
@@ -0,0 +1,40 @@
+import { GoldenPrimes, GoldenInverses } from '../../interfaces'
+
+export const primes26: GoldenPrimes = [
+  BigInt('2'),
+  BigInt('17'),
+  BigInt('419'),
+  BigInt('10867'),
+  BigInt('282427'),
+  BigInt('7343107'),
+  BigInt('190920451'),
+  BigInt('4963931759'),
+  BigInt('129062223683'),
+  BigInt('3355617815651'),
+  BigInt('87246063206927'),
+  BigInt('2268397643379331'),
+  BigInt('58978338727860347'),
+  BigInt('1533436806924367339'),
+  BigInt('39869356980033548923'),
+  BigInt('1036603281480872271857'),
+  BigInt('26951685318502679066009')
+]
+export const inverses26: GoldenInverses = [
+  BigInt('0'),
+  BigInt('23'),
+  BigInt('555'),
+  BigInt('4003'),
+  BigInt('200259'),
+  BigInt('6405355'),
+  BigInt('166406827'),
+  BigInt('2348544655'),
+  BigInt('99013302891'),
+  BigInt('1020941221707'),
+  BigInt('95795217660143'),
+  BigInt('479830684898347'),
+  BigInt('10033507223729843'),
+  BigInt('2312125289585707715'),
+  BigInt('58012575907549666483'),
+  BigInt('1204672436721283035409'),
+  BigInt('26042798102750233448105')
+]
