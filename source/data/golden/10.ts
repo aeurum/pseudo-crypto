3ebfeeb6c4cf325cfae4407d5aea3681d0b96689
Eaxon <aeurum@gmail.com>
2025-04-08 00:36:30 +0300

    Create 10.ts

---
+++ source/data/golden/10.ts
@@ -0,0 +1,40 @@
+import { GoldenPrimes, GoldenInverses } from '../../interfaces'
+
+export const primes10: GoldenPrimes = [
+  BigInt('2'),
+  BigInt('7'),
+  BigInt('67'),
+  BigInt('619'),
+  BigInt('6197'),
+  BigInt('61813'),
+  BigInt('618041'),
+  BigInt('6180341'),
+  BigInt('61803419'),
+  BigInt('618034003'),
+  BigInt('6180339923'),
+  BigInt('61803398903'),
+  BigInt('618033988751'),
+  BigInt('6180339887543'),
+  BigInt('61803398875019'),
+  BigInt('618033988749911'),
+  BigInt('6180339887498971')
+]
+export const inverses10: GoldenInverses = [
+  BigInt('0'),
+  BigInt('3'),
+  BigInt('3'),
+  BigInt('979'),
+  BigInt('1533'),
+  BigInt('30877'),
+  BigInt('519561'),
+  BigInt('9991261'),
+  BigInt('32652179'),
+  BigInt('270440667'),
+  BigInt('229552987'),
+  BigInt('71918074567'),
+  BigInt('37014448751'),
+  BigInt('8888268390407'),
+  BigInt('46560923756579'),
+  BigInt('34164613497191'),
+  BigInt('5187581788398931')
+]
