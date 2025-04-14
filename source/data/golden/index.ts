24d94c11d56df13f0e6661b012fdd79888ae4816
Eaxon <aeurum@gmail.com>
2025-04-14 13:23:46 +0300

    Added `thin` type

--- source/data/golden/index.ts
+++ source/data/golden/index.ts
@@ -1,4 +1,5 @@
 import { GoldenData } from '../../interfaces'
+import { primes3, inverses3 } from './3'
 import { primes10, inverses10 } from './10'
 import { primes13, inverses13 } from './13'
 import { primes21, inverses21 } from './21'
@@ -6,7 +7,8 @@ import { primes26, inverses26 } from './26'
 import { primes52, inverses52 } from './52'
 import { primes62, inverses62 } from './62'
 
-export const goldenData: Record<string, GoldenData> = {
+export const goldenData: Record<number, GoldenData> = {
+  3: { primes: primes3, inverses: inverses3 },
   10: { primes: primes10, inverses: inverses10 },
   13: { primes: primes13, inverses: inverses13 },
   21: { primes: primes21, inverses: inverses21 },
