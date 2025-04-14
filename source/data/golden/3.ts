23a15bca26767a27fe74b19213c388facfd6a923
Eaxon <aeurum@gmail.com>
2025-04-14 13:24:11 +0300

    Added `thin` type

---
+++ source/data/golden/3.ts
@@ -0,0 +1,40 @@
+import { GoldenPrimes, GoldenInverses } from '../../interfaces'
+
+export const primes3: GoldenPrimes = [
+  BigInt('2'),
+  BigInt('2'),
+  BigInt('7'),
+  BigInt('17'),
+  BigInt('53'),
+  BigInt('151'),
+  BigInt('457'),
+  BigInt('1361'),
+  BigInt('4057'),
+  BigInt('12197'),
+  BigInt('36497'),
+  BigInt('109507'),
+  BigInt('328481'),
+  BigInt('985351'),
+  BigInt('2956061'),
+  BigInt('8868137'),
+  BigInt('26604343')
+]
+export const inverses3: GoldenInverses = [
+  BigInt('0'),
+  BigInt('2'),
+  BigInt('4'),
+  BigInt('8'),
+  BigInt('26'),
+  BigInt('103'),
+  BigInt('67'),
+  BigInt('977'),
+  BigInt('5809'),
+  BigInt('9689'),
+  BigInt('1427'),
+  BigInt('40759'),
+  BigInt('528092'),
+  BigInt('492370'),
+  BigInt('4258625'),
+  BigInt('3341603'),
+  BigInt('42057253')
+]
